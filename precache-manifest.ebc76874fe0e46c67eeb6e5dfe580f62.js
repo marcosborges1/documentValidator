53df90ba4a17b228b209fcd92a9d2e12ddfe1654
marcosborges1 <borges.marcos1@gmail.com>
2021-01-18 22:40:09 -0300

    Updates

---
+++ precache-manifest.ebc76874fe0e46c67eeb6e5dfe580f62.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b7a0a42f80f291c14b36",
+    "url": "/documentValidator/static/css/main.ed69781d.chunk.css"
+  },
+  {
+    "revision": "b7a0a42f80f291c14b36",
+    "url": "/documentValidator/static/js/main.b7a0a42f.chunk.js"
+  },
+  {
+    "revision": "efe8d4eb94c710e8de66",
+    "url": "/documentValidator/static/css/1.e01f8f15.chunk.css"
+  },
+  {
+    "revision": "efe8d4eb94c710e8de66",
+    "url": "/documentValidator/static/js/1.efe8d4eb.chunk.js"
+  },
+  {
+    "revision": "d555ec7e5abc150125b0",
+    "url": "/documentValidator/static/js/runtime~main.d555ec7e.js"
+  },
+  {
+    "revision": "73476783ca0f7583198978de7073815b",
+    "url": "/documentValidator/static/media/0.73476783.jpg"
+  },
+  {
+    "revision": "43aa1eff28b9965c34d924de1d1b8aec",
+    "url": "/documentValidator/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.26edd2a61e45840263a67fda84058b98.js"
+  "/documentValidator/precache-manifest.ebc76874fe0e46c67eeb6e5dfe580f62.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("./index.html", {
+workbox.routing.registerNavigationRoute("/documentValidator/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
