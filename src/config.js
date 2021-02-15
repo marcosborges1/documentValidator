b6cc360a5b86e6fd29e4311b5ce15c268c435092
marcosborges1 <borges.marcos1@gmail.com>
2021-02-14 22:18:29 -0300

    Versão estável

--- src/config.js
+++ src/config.js
@@ -1,14 +1,14 @@
 const BASE_URL = `${window.location.origin}`
-const PORT = 4000
+const PORT = 8080
 
 
 if(BASE_URL.search("localhost") == -1) {
     module.exports = {
-        SERVER_URL:`http://localhost:${PORT}`    
+        SERVER_URL:`https://server-document-validator-12.uc.r.appspot.com`    
     } 
 }
 else {
     module.exports = {
-        SERVER_URL: `http://localhost:${PORT}`    
+        SERVER_URL: `http://localhost:${PORT}`
     }
 }
