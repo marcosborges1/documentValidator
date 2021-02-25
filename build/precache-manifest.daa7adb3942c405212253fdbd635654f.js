e419203f1408bf1edfec62648145ecc71250b725
marcosborges1 <borges.marcos1@gmail.com>
2021-02-25 02:26:37 -0300

    Nova versão ok

---
+++ build/precache-manifest.daa7adb3942c405212253fdbd635654f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "8e04e1ddaf344128309f",
+    "url": "./static/css/main.0b3f0f36.chunk.css"
+  },
+  {
+    "revision": "8e04e1ddaf344128309f",
+    "url": "./static/js/main.8e04e1dd.chunk.js"
+  },
+  {
+    "revision": "4d32d542d7e567b6d208",
+    "url": "./static/css/1.3f98c19e.chunk.css"
+  },
+  {
+    "revision": "4d32d542d7e567b6d208",
+    "url": "./static/js/1.4d32d542.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "./static/media/notification.c0d3c94c.eot"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "./static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "./static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "./static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "17ef9515df6b52a141ebbfbeec350366",
+    "url": "./index.html"
+  }
+];
